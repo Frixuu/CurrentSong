59cc37810eb22b96c425a8f524aeec58e08daf59
Frixuu <kontakt@lukasz.xyz>
2022-02-06 23:03:59 +0100

    Only refresh process info

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,26 @@
 use crossbeam_channel::TryRecvError;
 use std::{thread, time::Duration};
-use sysinfo::{PidExt, ProcessExt, System, SystemExt};
+use sysinfo::{PidExt, ProcessExt, ProcessRefreshKind, System, SystemExt};
 
 mod process;
 
 fn main() {
+    // Handle SIGINT, SIGTERM, etc. for graceful shutdown
     let (s, r) = crossbeam_channel::unbounded::<()>();
     ctrlc::set_handler(move || s.send(()).expect("Cannot send signal"))
         .expect("Cannot set handler");
 
     let mut system = System::new();
     loop {
-        system.refresh_all();
+        system.refresh_processes_specifics(ProcessRefreshKind::new());
         for process in system.processes_by_name("Spotify") {
             let pid = process.pid().as_u32();
             if let Some(title) = process::find_main_window_title(pid) {
                 println!("{}", title);
             }
         }
+
+        // Exit if message has been sent or disconnected from channel
         if let Err(TryRecvError::Empty) = r.try_recv() {
             thread::sleep(Duration::from_millis(2000));
         } else {
